768f966224e325b6c548ce49e29ee411b41d3515
Luke Ittycheria <littycheria2019@pingry.org>
2016-03-07 08:16:17 -0500

    Update tenCharacters.py

--- tenCharacters.py
+++ tenCharacters.py
@@ -2,13 +2,13 @@
 #Date: 3/1/16
 #Program: Ten Characters
 
-f = open("input.txt",'r')
-output = open("outputTen.txt",'w')
+f = open("input.txt",'r')#Open the input file
+output = open("outputTen.txt",'w')#Open the output file
 
-txtLines = f.readlines()
+txtLines = f.readlines() #Read the text
 
 for line in txtLines:
-	output.write(line[:10])
+	output.write(line[:10])#Write the new lines of code to the output file
 	output.write("\n")
 		
-f.close()
+f.close()#close file
